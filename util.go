35241d865371635703005302fa0c3d44fdbb92c2
Gábor Görzsöny <gabor@gorzsony.com>
2023-12-05 23:35:33 +0100

    Restrict access to private networks

--- util.go
+++ util.go
@@ -3,10 +3,58 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"net/url"
 	"strings"
 )
+
+func getPrivateIPBlocks() (blocks []*net.IPNet) {
+	// https://stackoverflow.com/a/50825191
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"169.254.0.0/16", // RFC3927 link-local
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+		"fc00::/7",       // IPv6 unique local addr
+	} {
+		_, block, err := net.ParseCIDR(cidr)
+		if err != nil {
+			panic(fmt.Errorf("parse error on %q: %v", cidr, err))
+		}
+		blocks = append(blocks, block)
+	}
+	return
+}
+
+var privateIPBlocks = getPrivateIPBlocks()
+
+func IsPrivateIP(ip net.IP) bool {
+	if ip.IsLoopback() || ip.IsLinkLocalUnicast() || ip.IsLinkLocalMulticast() {
+		return true
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
+
+func GetHost(u *url.URL) string {
+	host := u.Host
+	if strings.HasPrefix(host, "[") && strings.Contains(host, "]") {
+		host = host[1:strings.Index(host, "]")]
+	} else if strings.Contains(host, ":") {
+		parts := strings.Split(host, ":")
+		host = parts[0]
+	}
+	return host
+}
 
 func ToCamelCase(name string) string {
 	parts := strings.Split(name, "_")
@@ -17,6 +65,16 @@ func ToCamelCase(name string) string {
 }
 
 func DownloadJSON(u *url.URL) (string, error) {
+	ips, err := net.LookupIP(GetHost(u))
+	if err != nil {
+		return "", err
+	}
+	for _, ip := range ips {
+		if IsPrivateIP(ip) {
+			return "", fmt.Errorf("access to private networks is restricted")
+		}
+	}
+
 	response, err := http.Get(u.String())
 	if err != nil {
 		return "", err
